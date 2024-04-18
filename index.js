fc58b402cffeaddcd71970b1cb3ec7c7a4e3d907
Lumbagu <masiver.ae@yahoo.com>
2024-04-18 15:32:07 +0200

    my solution

--- index.js
+++ index.js
@@ -11,8 +11,12 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/hello", (req, res) => {
-    res.json({ greeting: "hello API" });
+app.get("/api/whoami", (req, res) => {
+    res.json({
+        ipaddress: req.ip,
+        language: req.headers["accept-language"],
+        software: req.headers["user-agent"]
+    });
 });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
